cdc7233542df824f8e61f81a2674e6ff7bacd22b
KJ Roelke <kj.roelke@gmail.com>
2023-03-31 11:58:30 -0500

    POST endpoint

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -15,12 +15,25 @@ const tours = JSON.parse(
  */
 
 const app = express();
+app.use(express.json());
 
 // REST Endpoints
 app.get(`${api}/tours`, (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: tours });
+});
+
+app.post(`${api}/tours`, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => console.error(err)
+  );
+  res.status(201).json({ status: 'success', data: { tour: newTour } });
 });
 
 // Start server
