a51ee57ce07ef324b3160e79986e1c7d6fe6db45
KJ Roelke <kj.roelke@gmail.com>
2023-03-31 11:48:45 -0500

    build rest api/v1 & get route

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,26 @@
 // Imports
 const express = require('express');
-const app = express();
+const fs = require('fs');
 
 // Utils
 const port = 8000;
+const api = `/api/v1`;
 
-// App
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Server-side Greetings!', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+/**
+ * APP
+ */
+
+const app = express();
+
+// REST Endpoints
+app.get(`${api}/tours`, (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: tours });
 });
 
 // Start server
