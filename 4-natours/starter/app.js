4663bc511acffad75408f9bbd12e4d4eb8d10467
KJ Roelke <kj.roelke@gmail.com>
2023-03-31 12:27:37 -0500

    finish routes and refactor

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -18,14 +18,23 @@ const app = express();
 app.use(express.json());
 
 // REST Endpoints
+function isValidTour(id) {
+  const tour = tours.find((tour) => tour.id === Number(id));
+  if (id > tours.length || !tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'tour not found! Check the id and try again.',
+    });
+  } else return true;
+}
 // GET
-app.get(`${api}/tours`, (req, res) => {
+function getAllTours(req, res) {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: tours });
-});
+}
 
-app.get(`${api}/tours/:id`, (req, res) => {
+function getTour(req, res) {
   const id = Number(req.params.id);
   const tour = tours.find((tour) => tour.id === id);
   if (id > tours.length || !tour) {
@@ -33,12 +42,14 @@ app.get(`${api}/tours/:id`, (req, res) => {
       status: 'fail',
       message: 'tour not found! Check the id and try again.',
     });
+  } else {
+    res.status(200).json({ status: 'success', data: tour });
   }
-  res.status(200).json({ status: 'success', data: tour });
-});
+}
 
 // POST
-app.post(`${api}/tours`, (req, res) => {
+
+function createTour(req, res) {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -48,7 +59,42 @@ app.post(`${api}/tours`, (req, res) => {
     (err) => console.error(err)
   );
   res.status(201).json({ status: 'success', data: { tour: newTour } });
-});
+}
+
+// PATCH
+
+function updateTour(req, res) {
+  const id = Number(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+  if (id > tours.length || !tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'tour not found! Check the id and try again.',
+    });
+  } else {
+    res
+      .status(200)
+      .json({ status: 'success', data: { tour: '<Updated Tour here>...' } });
+  }
+}
+
+// DELETE
+
+function deleteTour(req, res) {
+  const id = Number(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+  if (id > tours.length || !tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'tour not found! Check the id and try again.',
+    });
+  } else {
+    res.status(204).json({ status: 'success', data: null });
+  }
+}
+
+app.route(`${api}/tours`).get(getAllTours).post(createTour);
+app.route(`${api}/tours/:id`).get(getTour).patch(updateTour).delete(deleteTour);
 
 // Start server
 app.listen(8000, () => {
